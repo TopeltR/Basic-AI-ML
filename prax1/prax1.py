e256ec45e1c2637a17f57938b4c26faa6111d1df
Rasmus <rasmus@spiery.com>
2019-02-13 22:45:47 +0200

    First commit

--- prax1/prax1.py
+++ prax1/prax1.py
@@ -1,24 +1,22 @@
 from queue import Queue
 
 lava_map1 = [
-    "      **               **      ",
+    "                           ",
     "     ***     D        ***      ",
     "     ***                       ",
     "                      *****    ",
     "           ****      ********  ",
     "           ***          *******",
-    " **                      ******",
+    "                       ******",
     "*****             ****     *** ",
-    "*****              **          ",
+    "*****                        ",
     "***                            ",
-    "              **         ******",
-    "**            ***       *******",
+    "                       ******",
+    "            ***       *******",
     "***                      ***** ",
     "                               ",
     "                s              ",
 ]
-start_row=14
-start_col=16
 
 lava_map2 = [
     "     **********************    ",
@@ -37,35 +35,61 @@ lava_map2 = [
     "                               ",
     "                s              ",
 ]
+start_row = 14
+start_col = 16
+start = (start_col, start_row)
 
 
+def not_outside_map(neighbour, height, width):
+    return height > neighbour[1] >= 0 and width > neighbour[0] >= 0
 
 
+def is_not_lava(lava_map, neighbour):
+    return lava_map[neighbour[1]][neighbour[0]] is not "*"
 
-def minu_otsing(kaart):
-    # start (algolek) on näiteks tuple kujul (x, y)
-    start = (start_col,start_row)
+
+def draw_path(lava_map: list, path: list) -> None:
+    for coord in path[:-1]:
+        lava_map[coord[1]][coord[0]] = "."
+    print("\n".join(["".join(row) for row in lava_map]))
+
+
+def get_neighbours(location: tuple, lava_map: list) -> list:
+    x = location[0]
+    y = location[1]
+    neighbours = []
+    for y_neighbour in [-1, 0, 1]:
+        for x_neighbour in [-1, 0, 1]:
+            neighbour = (x + x_neighbour, y + y_neighbour)
+            if abs(y_neighbour) + abs(x_neighbour) == 1:
+                if not_outside_map(neighbour, len(lava_map), len(lava_map[0])):
+                    if is_not_lava(lava_map, neighbour):
+                        neighbours.append(neighbour)
+    return neighbours
+
+
+def minu_otsing(map, start):
+    map = [list(row) for row in map]
     frontier = Queue()
     frontier.put(start)
-    came_from = {}
-    came_from[start] = None
-    path = []
+    came_from = {start: None}
     while not frontier.empty():
         current = frontier.get()
-        if current == 'B' :
-            path.append(current)
+        if map[current[1]][current[0]] == 'D':
             break
+        for next in get_neighbours(current, map):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = current
+    path = []
+    while came_from[current] is not None:
+        path.append(current)
+        current = came_from[current]
 
-       # for next in graph.neighbors(current):  # see osa tuleb suht palju ümber teha.
-                                               # tuleb leida sobivad naaberruudud kaardilt
-                                               # nagu ta meile ette on antud (ülal, all,
-                                               # paremal ja vasakul olev ruut)
-            # if next not in came_from:
-            #     frontier.put(next)
-            #     came_from[next] = current
+    path.reverse()
 
-
-    # Kui teemant on leitud, tuleb ka teekond rekonstrueerida
-    # mis andmestruktuurina teekonda esitada, pole oluline,
-    # aga loomulik viis oleks list
+    draw_path(map, path)
     return path
+
+
+minu_otsing(lava_map2, start)
